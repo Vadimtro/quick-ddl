68b194136110e87d18dc1eec96586e074b2080c9
Vadim.tropashko@oracle.com <Vadim.tropashko@oracle.com>
2023-04-27 16:33:39 -0700

    fixed erroring on ill defined view

--- js/tree.js
+++ js/tree.js
@@ -498,6 +498,9 @@ var tree = (function(){
             ret += 'select\n';
             var maxLen = 0;
             for( var i = 2; i < chunks.length; i++ ) { 
+                var tbl = ddl.find(chunks[i]);
+                if( tbl == null )
+                    return "";
                 var len = (chunks[i]+'.ID').length;
                 if( maxLen < len )
                     maxLen = len;
